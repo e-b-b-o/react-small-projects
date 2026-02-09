9347e86c3686fe0bcea8aeeb11d63f4ed64bf07e
Ebisa Getachew <ebisagetachew1243@gmail.com>
2026-02-09 22:46:49 +0300

    little improvement

--- 03-travel-list/src/App.js
+++ 03-travel-list/src/App.js
@@ -1,3 +1,8 @@
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: false },
+];
+
 export default function App() {
   return (
     <div className="app">
@@ -21,10 +26,27 @@ function Form() {
 }
 
 function PackageList() {
-  return <div className="list">LIST</div>;
+  return (
+    <div className="list">
+      <ul>
+        {initialItems.map((item) => (
+          <Item item={item} />
+        ))}
+      </ul>
+    </div>
+  );
 }
-
-function Stats() {
+function Item({ item }) {
+  return (
+    <li>
+      <span style={item.packed ? { textDecoration: "line-through" } : {}}>
+        {item.quantity} {item.description}
+      </span>
+      <button>❌</button>
+    </li>
+  );
+}
+function Stats() { 
   return (
     <footer className="stats">
       <em> 👜You have X items on your list, and you already packed x (X%) </em>
