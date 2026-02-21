01681f12ba42503672ec61b91927cf08aea32eea
Ebisa Getachew <ebisagetachew1243@gmail.com>
2026-02-21 16:11:59 +0300

    added new project

---
+++ 06-usepopcorn/src/index.js
@@ -0,0 +1,28 @@
+import React, { useState } from "react";
+import ReactDOM from "react-dom/client";
+// import './index.css';
+// import App from './App';
+import StarRating from "./StarRating";
+
+function Test() {
+  const [movieRating, setMovieRating] = useState(0);
+  return (
+    <div>
+      <StarRating onSetRating={setMovieRating} color="blue" maxRating={10} />
+      <p>this movie rating was {movieRating}</p>
+    </div>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <Test />
+    <StarRating maxRating={5} />
+    <StarRating
+      maxRating={5}
+      messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
+    />
+    <StarRating maxRating={10} color="red" size="20" defaultRating="7" />
+  </React.StrictMode>,
+);
