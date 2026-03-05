a2026e3badd968b3566bbeaa6546faf14e2aa323
Ebisa Getachew <ebisagetachew1243@gmail.com>
2026-03-05 23:26:36 +0300

    added new features

--- 06-usepopcorn/src/index.js
+++ 06-usepopcorn/src/index.js
@@ -1,29 +1,12 @@
 import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
-import './index.css';
+import "./index.css";
 import App from "./App";
 import StarRating from "./StarRating";
-
-function Test() {
-  const [movieRating, setMovieRating] = useState(0);
-  return (
-    <div>
-      <StarRating onSetRating={setMovieRating} color="blue" maxRating={10} />
-      <p>this movie rating was {movieRating}</p>
-    </div>
-  );
-}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
-    <Test />
-    <StarRating maxRating={5} />
-    <StarRating
-      maxRating={5}
-      messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
-    />
-    <StarRating maxRating={10} color="red" size={48} defaultRating={7} />
   </React.StrictMode>,
 );
